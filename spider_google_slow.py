b20c579c9e6ed2053124e91c863f7ec55d1b1342
chenhongming <m18435153930@163.com>
2018-04-25 10:09:02 +0800

    Update spider_google_slow.py

--- spider_google_slow.py
+++ spider_google_slow.py
@@ -1,10 +1,3 @@
-# *****************************
-# ****** 2018 3.14  ***********
-# *****  Author: LQ  **********
-# ****** Python 3.6.3 *********
-# **** 用于爬取谷歌的图片********
-# *****************************
-
 from selenium import webdriver
 import time
 import urllib
@@ -14,48 +7,43 @@ import os
 
 # ****************************************************
 base_url_part1 = 'https://www.google.com/search?q='
-base_url_part2 = '&source=lnms&tbm=isch'  # base_url_part1以及base_url_part2都是固定不变的，无需更改
-search_query = '旗风级驱逐舰'  # 检索的关键词，可自己输入你想检索的关键字
-location_driver = '/usr/local/bin/chromedriver'  # Chrome驱动程序在电脑中的位置
+base_url_part2 = '&source=lnms&tbm=isch'  # base_url_part1 and base_url_part2 are fixed and there's no need to change
+search_query = 'keywords'  # Search keywords, you can enter the keywords you want to retrieve
+location_driver = '/usr/local/bin/chromedriver'  # This is a necessary Google driver for Mac
+#location_driver = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe' # This is a necessary Google driver for Windowns
+          
 
 
 class Crawler:
     def __init__(self):
         self.url = base_url_part1 + search_query + base_url_part2
 
-    # 启动Chrome浏览器驱动
+    # Launch Chrome browser driver
     def start_brower(self):
-        # 启动Chrome浏览器
         driver = webdriver.Chrome(location_driver)
-        # 最大化窗口，因为每一次爬取只能看到视窗内的图片
         driver.maximize_window()
-        # 浏览器打开爬取页面
         driver.get(self.url)
         return driver
 
     def downloadImg(self, driver):
         t = time.localtime(time.time())
         foldername = str(t.__getattribute__("tm_year")) + "-" + str(t.__getattribute__("tm_mon")) + "-" + str(
-            t.__getattribute__("tm_mday"))  # 定义文件夹的名字
-        picpath = '/Users/chenhongming/images/%s' % (foldername)  # 下载到的本地目录
-        if not os.path.exists(picpath):  # 路径不存在时创建一个
+            t.__getattribute__("tm_mday"))  # Define the folder name
+        picpath = '/Users/chenhongming/images/%s' % (foldername)  # Download to local directory
+        if not os.path.exists(picpath):
             os.makedirs(picpath)
 
-        # 记录下载过的图片地址，避免重复下载
+        # Record downloaded picture address to avoid duplicate pictures
         img_url_dic = {}
         x = 000
-        # 当鼠标的位置小于最后的鼠标位置时,循环执行
         pos = 0
-        for i in range(1, 6):  # 此处可自己设置爬取范围
-            pos = i * 500  # 每次下滚500
+        for i in range(1, 6):  # Here you can set the crawl range for yourself
+            pos = i * 500
             js = "document.documentElement.scrollTop=%d" % pos
             driver.execute_script(js)
             time.sleep(2)
-            # 获取页面源码
             html_page = driver.page_source
-            # 利用Beautifulsoup4创建soup对象并进行页面解析
             soup = bs(html_page, "html.parser")
-            # 通过soup对象中的findAll函数图像信息提取
             imglist = soup.findAll('img', {'class': 'rg_ic rg_i'})
 
             for imgurl in imglist:
@@ -63,7 +51,7 @@ class Crawler:
                     print(x, end=' ')
                     if imgurl['src'] not in img_url_dic:
                         target = picpath + '\\%s.jpg' % x
-                        # print ('Downloading image to location: ' + target + '\nurl=' + imgurl['src'])
+                        print ('Downloading image to location: ' + target)
                         img_url_dic[imgurl['src']] = ''
                         urllib.request.urlretrieve(imgurl['src'], target)
                         time.sleep(1)
@@ -73,9 +61,7 @@ class Crawler:
                     break
 
     def run(self):
-        print('''      ************************************* 
-        **      Welcome to use Spider      ** 
-        *************************************''')
+        print('Downloading image')
 
         driver = self.start_brower()
         self.downloadImg(driver)
